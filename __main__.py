01daf3c887e289cc6650bc0130cece0b0ce804fb
Joshua Peek <josh@joshpeek.com>
2021-07-29 17:03:07 -0700

    Summarize logging

--- __main__.py
+++ __main__.py
@@ -1,24 +1,28 @@
-import os
-
-from tqdm import tqdm
+import sys
 
 import plex_library
 import trakt_played
 
 watched_guids = trakt_played.watched_guids()
 
+watched_count = 0
+unwatched_count = 0
 
-for (guids, video) in tqdm(
-    plex_library.videos(), total=plex_library.totalSize(), disable=os.getenv("CI")
-):
+for (guids, video) in plex_library.videos():
     if not guids:
         continue
 
     isWatched = watched_guids.intersection(guids)
 
     if video.isWatched and not isWatched:
-        print("+", video.title)
         video.markUnwatched()
+        unwatched_count += 1
     elif not video.isWatched and isWatched:
-        print("-", video.title)
         video.markWatched()
+        watched_count += 1
+
+if watched_count:
+    print("+{} watched".format(watched_count), file=sys.stderr)
+
+if unwatched_count:
+    print("-{} unwatched".format(unwatched_count), file=sys.stderr)
