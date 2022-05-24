9ca6825fc241be3f7a84c6f368dbc02502730247
Joshua Peek <josh@users.noreply.github.com>
2022-05-24 18:11:35 +0000

    Try fetching guid from my index

--- plex_cache.py
+++ plex_cache.py
@@ -1,4 +1,5 @@
 import plexapi
+import requests
 
 import cache
 
@@ -37,4 +38,21 @@ def video_guids2(account, video):
 
 def plex_guid(guid):
     assert isinstance(guid, str)
-    return cache.get(guid)
+
+    external_guid = cache.get(guid)
+    if external_guid:
+        assert isinstance(external_guid, str)
+        return external_guid
+
+    if guid.startswith("tmdb://"):
+        id = int(guid[7:])
+        url = "https://josh.github.io/pmdb/tmdb/movie/{}.json".format(id)
+        r = requests.get(url)
+        r.raise_for_status()
+        data = r.json()
+        if data.get("plex_type") and data.get("plex_id"):
+            plex_guid = "plex://{}/{}".format(data["plex_type"], data["plex_id"])
+            cache.set(guid, plex_guid)
+            return plex_guid
+
+    return None
