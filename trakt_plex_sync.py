6987d2d07ec7eb0aac561211cc197cdcd5c83bd5
Joshua Peek <josh@users.noreply.github.com>
2024-12-27 21:59:52 -0800

    Move into main function

--- trakt_plex_sync.py
+++ trakt_plex_sync.py
@@ -3,26 +3,32 @@ import sys
 import plex_library
 import trakt_played
 
-watched_guids = trakt_played.watched_guids()
 
-watched_count = 0
-unwatched_count = 0
+def main() -> None:
+    watched_guids = trakt_played.watched_guids()
 
-for guids, video in plex_library.videos():
-    if not guids:
-        continue
+    watched_count = 0
+    unwatched_count = 0
 
-    isPlayed = watched_guids.intersection(guids)
+    for guids, video in plex_library.videos():
+        if not guids:
+            continue
 
-    if video.isPlayed and not isPlayed:
-        video.markUnwatched()
-        unwatched_count += 1
-    elif not video.isPlayed and isPlayed:
-        video.markPlayed()
-        watched_count += 1
+        isPlayed = watched_guids.intersection(guids)
 
-if watched_count:
-    print(f"+{watched_count} watched", file=sys.stderr)
+        if video.isPlayed and not isPlayed:
+            video.markUnwatched()
+            unwatched_count += 1
+        elif not video.isPlayed and isPlayed:
+            video.markPlayed()
+            watched_count += 1
 
-if unwatched_count:
-    print(f"-{unwatched_count} unwatched", file=sys.stderr)
+    if watched_count:
+        print(f"+{watched_count} watched", file=sys.stderr)
+
+    if unwatched_count:
+        print(f"-{unwatched_count} unwatched", file=sys.stderr)
+
+
+if __name__ == "__main__":
+    main()
