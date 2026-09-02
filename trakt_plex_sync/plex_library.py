f9bf6f2fa5f8ce2dbab89b6cc615ecd7141a1e28
Joshua Peek <josh@users.noreply.github.com>
2026-09-02 16:11:14 -0700

    Inline guid cache and drop lru-cache dependency

--- trakt_plex_sync/plex_library.py
+++ trakt_plex_sync/plex_library.py
@@ -1,10 +1,46 @@
 import os
-from collections.abc import Iterator
+import pickle
+from collections import OrderedDict
+from collections.abc import Callable, Iterator
+from contextlib import closing
 from functools import partial
+from pathlib import Path
 
-import lru_cache
 from plexapi.myplex import MyPlexAccount
 from plexapi.video import Video
+
+
+class _GuidCache:
+    """Video guid to external guids, persisted as a pickle.
+
+    Oldest entries are evicted on close until the pickle fits in max_bytesize.
+    """
+
+    _filename: Path
+    _max_bytesize: int
+    _data: OrderedDict[str, set[str]]
+
+    def __init__(self, filename: Path | str, max_bytesize: int) -> None:
+        self._filename = Path(filename)
+        self._max_bytesize = max_bytesize
+        self._data = OrderedDict()
+        if self._filename.exists():
+            self._data.update(pickle.loads(self._filename.read_bytes()))
+
+    def get_or_load(self, key: str, load_value: Callable[[], set[str]]) -> set[str]:
+        if key in self._data:
+            self._data.move_to_end(key)
+            return self._data[key]
+        self._data[key] = value = load_value()
+        return value
+
+    def close(self) -> None:
+        data = pickle.dumps(self._data, pickle.HIGHEST_PROTOCOL)
+        while len(data) > self._max_bytesize and self._data:
+            self._data.popitem(last=False)
+            data = pickle.dumps(self._data, pickle.HIGHEST_PROTOCOL)
+        self._filename.parent.mkdir(parents=True, exist_ok=True)
+        self._filename.write_bytes(data)
 
 
 def _video_guids(video: Video) -> set[str]:
@@ -20,12 +56,12 @@ def videos() -> Iterator[tuple[set[str], Video]]:
     resource = account.resource(os.environ["PLEX_SERVER"])  # type: ignore[no-untyped-call]
     plex = resource.connect()
 
-    cache = lru_cache.open(
+    cache = _GuidCache(
         os.environ.get("CACHE_PATH", "/tmp/cache.pickle"),
         max_bytesize=5 * 1024 * 1024,  # 5 MB
     )
 
-    with cache:
+    with closing(cache):
         for movie in plex.library.section("Movies").all():
             guids = cache.get_or_load(movie.guid, partial(_video_guids, movie))
             yield (guids, movie)
